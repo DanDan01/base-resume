c95d910fcfec2181402753dc5769eb533ffb38ca
DanDan01 <djdand1@gmail.com>
2022-08-28 09:45:09 -0500

    added title and page layout

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,39 @@
 <template>
   <div id="app">
-  <Jobs />
+    <header class="header">
+      <div class="header-container">
+        <div class="title">
+          <h3>Logo</h3>
+        </div>
+        <div class="navigation-container">
+          <nav>
+            <ul>
+              <li>Summary</li>
+              <li>Hobbies</li>
+              <li>Job History</li>
+              <li>Education</li>
+              <li>Certifications</li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+    <section class="Summary">
+      <h2>Summary</h2>
+    </section>
+    <section class="hobbies">
+      <h2>Hobbies</h2>
+    </section>
+    <section class="job-history">
+      <h2>Job History</h2>
+      <Jobs />
+    </section>
+    <section class="education">
+      <h2>Education</h2>
+    </section>
+    <section class="certifications">
+      <h1>Certifications</h2>
+    </section>
   </div>
 </template>
 
--- src/components/Jobs.vue
+++ src/components/Jobs.vue
@@ -1,21 +1,26 @@
 <template>
-  <h1>This is a test</h1>
+  <div class="jobs-container" v-for="jobs of work" :key="jobs.order">
+    <div class="title">Title: {{jobs.title}}</div>
+    <div class="start-date">Start Date: {{jobs.startDate}}</div>  
+    <div class="end-date">End Date: {{jobs.endDate}}</div>
+    <div class="city">City: {{jobs.city}}</div> 
+    <div class="state">State: {{jobs.state}}</div>
+    <div class="summary">Summary: {{jobs.summary}}</div>
+  </div>
 </template>
 
 <script>
-  
+  import { computed, ref } from 'vue';
 
   export default {
     components: 'Jobs',
     data(){
       return {
-        computed: {
-          work: [
-            {title: "Title", startDate: "Dates", endDate: "End Date", city: "City", summary: "Summary"}
-          ]
-        }
+         work: ref([
+            {order: "1", currentRole: false, title: "Title", startDate: "Dates", endDate: "End Date", city: "City", state: "State", summary: "Summary"}
+        ])
       }
-    },
+    }
   }
 </script>
 
